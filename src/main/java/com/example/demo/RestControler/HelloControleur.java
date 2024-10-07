cd6116fb32e8e7eb12c891163ad87642c0045451
Cyril <cyril.personne@outlook.fr>
2024-10-07 16:16:38 +0200

    first commit

---
+++ src/main/java/com/example/demo/RestControler/HelloControleur.java
@@ -0,0 +1,21 @@
+package com.example.demo.Controler;
+
+import com.example.demo.Service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/hello")
+public class HelloControleur {
+
+    @Autowired
+    private HelloService service;
+
+    @GetMapping
+    public String direHello() {
+        return service.salutations();
+    }
+
+}
